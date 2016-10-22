ed9923e6ccc79270ed1514412d4de41dc7e0ef0f
Faeez Shaikh <faeezshaikh@Faeezs-MacBook-Pro.local>
2016-10-21 22:37:34 -0500

    Removed search and browse pages

--- www/js/app.js
+++ www/js/app.js
@@ -95,23 +95,6 @@ angular.module('starter', ['ionic',  'starter.controllers', 'auth0', 'angular-st
         }
       })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
     .state('app.playlists', {
       url: '/playlists',
       views: {
