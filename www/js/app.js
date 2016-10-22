5d5484ad413026609330a53d46009c62c3a0445a
Faeez Shaikh <faeezshaikh@Faeezs-MacBook-Pro.local>
2016-10-21 21:27:05 -0500

    Removing circularNavigstion Module

--- www/js/app.js
+++ www/js/app.js
@@ -4,8 +4,8 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic',  'ionic.cloud','starter.controllers', 'auth0', 'angular-storage',
-     'ngCordova', 'firebase','angularMoment','angular-storage','ngMap','angularCircularNavigation'])
+angular.module('starter', ['ionic',  'starter.controllers', 'auth0', 'angular-storage',
+     'ngCordova', 'firebase','angularMoment','angular-storage','ngMap'])
 
 .constant('FIREBASE_URL','https://homelesscare.firebaseio.com/')    
 
@@ -40,7 +40,7 @@ angular.module('starter', ['ionic',  'ionic.cloud','starter.controllers', 'auth0
 	    }
 	  });
 
-    
+
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
